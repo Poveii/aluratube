34a229c1ced7588a90be9aad7264ae77023b920c
Poveii <yellowtutors@gmail.com>
2022-11-09 10:10:48 -0300

    :lipstick: feat: Styled Header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import config from "../config.json"
+import styled from "styled-components"
 
 function HomePage() {
   const homePageStyles = { backgroundColor: "red" }
@@ -18,14 +19,37 @@ function Menu() {
   return <div>Menu</div>
 }
 
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+
+  .user-info {
+    width: 100%;
+    padding: 16px 32px;
+
+    display: flex;
+    align-items: center;
+    gap: 16px;
+  }
+`
 function Header() {
   return (
-    <div>
+    <StyledHeader>
       {/* <img src="" alt="" /> */}
-      <img src={`https://github.com/${config.github}.png`} />
-      <h2>{config.name}</h2>
-      <p>{config.job}</p>
-    </div>
+      <section className="user-info">
+        <img
+          src={`https://github.com/${config.github}.png`}
+          alt="Foto do Pablo Gabriel olhando pra frente"
+        />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
   )
 }
 

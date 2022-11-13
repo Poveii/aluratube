db58da3ca7cb78bb6d26fd2669fb31d4d4bb37fb
Poveii <yellowtutors@gmail.com>
2022-11-13 10:48:28 -0300

    :ok_hand: style: theme out MyApp component

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,23 @@ import ColorModeProvider, {
   ColorModeContext,
 } from "../src/components/Menu/components/ColorMode"
 
+const theme = {
+  light: {
+    backgroundBase: "#f9f9f9",
+    backgroundLevel1: "#ffffff",
+    backgroundLevel2: "#f0f0f0",
+    borderBase: "#e5e5e5",
+    textColorBase: "#222222",
+  },
+  dark: {
+    backgroundBase: "#181818",
+    backgroundLevel1: "#202020",
+    backgroundLevel2: "#313131",
+    borderBase: "#383838",
+    textColorBase: "#FFFFFF",
+  },
+}
+
 function ProviderWrapper(props) {
   return (
     <ColorModeProvider initialMode={"light"}>
@@ -14,23 +31,6 @@ function ProviderWrapper(props) {
 }
 
 function MyApp({ Component, pageProps }) {
-  const theme = {
-    light: {
-      backgroundBase: "#f9f9f9",
-      backgroundLevel1: "#ffffff",
-      backgroundLevel2: "#f0f0f0",
-      borderBase: "#e5e5e5",
-      textColorBase: "#222222",
-    },
-    dark: {
-      backgroundBase: "#181818",
-      backgroundLevel1: "#202020",
-      backgroundLevel2: "#313131",
-      borderBase: "#383838",
-      textColorBase: "#FFFFFF",
-    },
-  }
-
   const context = React.useContext(ColorModeContext)
 
   return (

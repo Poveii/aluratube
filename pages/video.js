55f5a31be9d9d049648e13446cea4a707a3328c1
Poveii <yellowtutors@gmail.com>
2023-04-03 10:39:37 -0300

    feat: Style Video Page

--- pages/video.js
+++ pages/video.js
@@ -1,14 +1,28 @@
-import React from "react"
-import { ColorModeContext } from "../src/components/Menu/components/ColorMode"
+import styled from "styled-components"
+import DarkModeSwitch from "../src/components/Menu/components/DarkModeSwitch"
+
+const StyledVideoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
+  width: 100%;
+`
 
 export default function Video() {
-  const context = React.useContext(ColorModeContext)
-
   return (
-    <div>
-      <h1>Video</h1>
-      <p>{context.mode}</p>
-      <button onClick={() => context.toggleMode()}>trocar modo</button>
-    </div>
+    <StyledVideoWrapper>
+      <a href="/">← Voltar</a>
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <DarkModeSwitch />
+    </StyledVideoWrapper>
   )
 }

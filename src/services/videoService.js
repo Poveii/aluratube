7dc40b4cde4ec4f4565fe34354e10aea497d4ba2
Poveii <yellowtutors@gmail.com>
2023-04-03 09:41:29 -0300

    refactor: Secure Supabase API keys

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
-const PROJECT_URL = "https://ygcgjbgzxtmuzzjhzcpz.supabase.co"
-const PUBLIC_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnY2dqYmd6eHRtdXp6amh6Y3B6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg0MjMxMDEsImV4cCI6MTk4Mzk5OTEwMX0.4rKHch7tnIKhjz8EgI_vpj-_Gj4kspidagFT1yEY41A"
+const PROJECT_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
+const PUBLIC_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
 const supabase = createClient(PROJECT_URL, PUBLIC_KEY)
 
 export function videoService() {
